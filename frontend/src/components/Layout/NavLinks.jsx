9ac4987e6e4fb30fb1c907f10278751abe3455f4
Kaelan Smith <kaelancsmith@gmail.com>
2023-08-28 09:29:16 -0700

    Remove Framer Motion

--- frontend/src/components/Layout/NavLinks.jsx
+++ frontend/src/components/Layout/NavLinks.jsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import { Link } from '@/components/Link'
-import { AnimatePresence, motion } from 'framer-motion'
+import { Link } from '@/components/Link';
+import classNames from '@/utils/classNames';
+import { useGlobals } from 'cloakwp';
 
 export function NavLinks({ links }) {
-  let [hoveredIndex, setHoveredIndex] = useState(null)
-  console.log({links})
-  return links?.map(({title, url, id}, index) => (
-    <Link
-      key={id}
-      href={url}
-      className="relative -my-2 -mx-3 rounded-full px-3 py-1 text-gray-800 text-lg transition-colors delay-150 hover:text-gray-100 hover:delay-[0ms]"
-      onMouseEnter={() => setHoveredIndex(index)}
-      onMouseLeave={() => setHoveredIndex(null)}
-    >
-      <AnimatePresence>
-        {hoveredIndex === index && (
-          <motion.span
-            className="absolute inset-0 bg-gray-900 rounded-full"
-            layoutId="hoverBackground"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1, transition: { duration: 0.15 } }}
-            exit={{
-              opacity: 0,
-              transition: { duration: 0.15, delay: 0.2 },
-            }}
-          />
+  const { pageData } = useGlobals();
+
+  return links?.map(({ title, url, id }) => {
+    const isCurrentPage = pageData?.pathname == url;
+    return (
+      <Link
+        key={id}
+        href={url}
+        className={classNames(
+          'relative -mx-3 -my-2 rounded-full px-3 py-1 text-base 2xl:text-lg transition-colors delay-150 hover:bg-gray-900 hover:text-gray-100 hover:delay-[0ms]',
+          isCurrentPage ? 'font-semibold text-gray-800' : 'text-gray-600'
         )}
-      </AnimatePresence>
-      <span className="relative z-10">{title}</span>
-    </Link>
-  ))
+      >
+        {title}
+      </Link>
+    );
+  });
 }
