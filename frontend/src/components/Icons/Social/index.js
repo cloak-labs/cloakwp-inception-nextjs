14401ad507ae093fba3b0a9c9b27667ae0d24951
Kaelan Smith <kaelancsmith@gmail.com>
2023-08-28 11:29:09 -0700

    format icon files and add LinkedIn and YouTube icons

--- frontend/src/components/Icons/Social/index.js
+++ frontend/src/components/Icons/Social/index.js
@@ -1,3 +1,5 @@
 export { Facebook } from './Facebook'
 export { Instagram } from './Instagram'
 export { Twitter } from './Twitter'
+export { LinkedIn } from './LinkedIn'
+export { YouTube } from './YouTube'
