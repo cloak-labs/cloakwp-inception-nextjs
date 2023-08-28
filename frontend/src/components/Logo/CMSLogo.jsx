20749daf6a288d892d25460f683c71b1f10884b0
Kaelan Smith <kaelancsmith@gmail.com>
2023-08-28 09:29:30 -0700

    Add CMSLogo component pulling from ACF Options data

---
+++ frontend/src/components/Logo/CMSLogo.jsx
@@ -0,0 +1,27 @@
+import { useGlobals } from 'cloakwp'
+import { Logo } from '@/components/Logo';
+import Image from 'next/future/image'
+import classNames from '@/utils/classNames';
+
+
+export default function CMSLogo({ className, imgClassName, onDark = false, ...props }) {
+  const { options } = useGlobals()
+  const logo = onDark ? options?.logo_on_dark : options?.logo
+  return (
+    <>
+      {logo?.url ? (
+        <div className={classNames('relative min-w-[200px] w-auto h-8', className)}>
+          <Image
+            src={logo.url}
+            alt={logo.alt}
+            fill
+            className={classNames('object-contain object-left', imgClassName)}
+            {...props}
+          />
+        </div>
+      ) : (
+        <Logo onDark={onDark} />
+      )}
+    </>
+  )
+}
