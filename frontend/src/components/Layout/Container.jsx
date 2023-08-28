9632945501411527cd36171771239d8e2aebc465
Kaelan Smith <kaelancsmith@gmail.com>
2023-08-28 11:29:20 -0700

    add 'as' prop to Container

--- frontend/src/components/Layout/Container.jsx
+++ frontend/src/components/Layout/Container.jsx
@@ -1,11 +1,11 @@
 import classNames from '@/utils/classNames';
 
-export function Container({ className, innerClassName, children, ...props }) {
+export function Container({ className, innerClassName, children, as: Element = 'div', ...props }) {
   const defaultInnerClassNames = 'px-4 sm:px-6 lg:px-9 mx-auto max-w-7xl lg:max-w-8xl'
   const hasBgColor = className?.includes('bg-') || false
 
   return (
-    <div
+    <Element
       className={classNames(
         !hasBgColor && defaultInnerClassNames,
         !hasBgColor && innerClassName,
@@ -19,6 +19,6 @@ export function Container({ className, innerClassName, children, ...props }) {
         </div>
       ) : children
       }
-    </div>
+    </Element>
   )
 }
