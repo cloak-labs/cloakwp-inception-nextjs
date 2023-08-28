00c1e429f32a9172ea948966d8d1423467b6e899
Kaelan Smith <kaelancsmith@gmail.com>
2023-08-28 09:30:31 -0700

    add GlobalsProvider, ErrorPage, etc.

--- frontend/src/pages/_app.jsx
+++ frontend/src/pages/_app.jsx
@@ -1,29 +1,50 @@
 import '@/styles/globals.css'
 import 'focus-visible'
-import { Layout } from '@/components/Layout'
-import { BlockConfigProvider } from 'cloakwp'
-import myBlockConfig from '@/config/myBlockConfig'
 import Head from 'next/head'
 import parse from 'html-react-parser';
+import { BlockConfigProvider, ErrorPage, GlobalsProvider } from 'cloakwp'
+import myBlockConfig from '@/config/myBlockConfig'
+import { Layout } from '@/components/Layout'
 
 export default function App({ Component, pageProps }) {
-  const { enableLayout = true, pageData } = pageProps
+  const {
+    enableLayout = true,
+    pageData,
+    navBarData = {},
+    options = {},
+    isPreview = false,
+    previewParams
+  } = pageProps
   const { yoast_head } = pageData || {}
 
   return (
-    <BlockConfigProvider blocks={myBlockConfig}>
-      {yoast_head &&
-        <Head>
-          {parse(yoast_head)}
-        </Head>
-      }
-      {enableLayout ? (
-        <Layout {...pageProps}>
-          <Component {...pageProps} />
+    <GlobalsProvider
+      options={options}
+      navBarData={navBarData}
+      pageData={pageData}
+      isPreview={isPreview}
+      previewParams={previewParams}
+    >
+      {pageData?.data?.status == 403 ? (
+        <Layout>
+          <ErrorPage errorData={pageData} />
         </Layout>
       ) : (
-        <Component {...pageProps} />
+        <BlockConfigProvider blocks={myBlockConfig}>
+          {yoast_head &&
+            <Head>
+              {parse(yoast_head)}
+            </Head>
+          }
+          {enableLayout ? (
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          ) : (
+            <Component {...pageProps} />
+          )}
+        </BlockConfigProvider>
       )}
-    </BlockConfigProvider>
+    </GlobalsProvider>
   )
 }
