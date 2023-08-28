3fbd80fa43dfc774f82dd7c4bb89478ca7914577
Kaelan Smith <kaelancsmith@gmail.com>
2023-08-28 09:29:00 -0700

    Add AdminBar component

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -1,14 +1,24 @@
 import classNames from "@/utils/classNames";
 import { Footer, Header } from "@/components/Layout";
+import { AdminBar, useGlobals } from "cloakwp";
 
-export function Layout({navBarData, children}) {
+export function Layout({ children }) {
+  const { pageData, isPreview, previewParams } = useGlobals()
+
   return (
-    <>
-      <Header navBarData={navBarData} />
+    <div
+      className="flex flex-col min-h-screen"
+    >
+      <AdminBar
+        isPreview={isPreview}
+        previewParams={previewParams}
+        pageData={pageData}
+      />
+      <Header />
       <main className={classNames("w-full max-w-none overflow-hidden min-h-[300px]")}>
         {children}
       </main>
-      <Footer navBarData={navBarData} />
-    </>
+      <Footer />
+    </div>
   )
 }
