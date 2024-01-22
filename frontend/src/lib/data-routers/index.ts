9f083403bdc44dee51db2212c5bc9875803db38d
Kaelan Smith <kaelancsmith@gmail.com>
2024-01-21 21:46:12 -0800

    add faqsDataRouter

--- frontend/src/lib/data-routers/index.ts
+++ frontend/src/lib/data-routers/index.ts
@@ -4,4 +4,5 @@ export { heroWithImageDataRouter } from './heroWithImageDataRouter';
 export { postsDataRouter } from './postsDataRouter';
 export { testimonialsDataRouter } from './testimonialsDataRouter';
 export { testimonialsCarouselDataRouter } from './testimonialsCarouselDataRouter';
+export { faqsDataRouter } from './faqsDataRouter';
 export { genericStylesDataRouter } from './genericStylesDataRouter';
